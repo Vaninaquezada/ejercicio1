2c749d8cec8a91bb99db6fcbfd59999c43930db5
Vanina Quezada <vaninaquezada@gmail.com>
2021-03-18 22:42:46 -0300

    fix 2

--- src/app/server.js
+++ src/app/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path') 
-app.use(express.static(__dirname + '/my-dream-app'));
+app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/my-dream-app/index.html'));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 console.log('Finalizo!!');
