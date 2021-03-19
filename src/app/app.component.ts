99cb55dcc1814ecc2bf742f5fd6e27dc0b90bc26
Vanina Quezada <vaninaquezada@gmail.com>
2021-03-18 21:18:15 -0300

    Ejecicio 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-dream-app';
+  title = 'Ejercicio 1';
+  edadUno:number =0;
+  edadDos:number =0;
+
+  resSuma:number =0;
+  resPromedio:number =0;
+
+  promedio():void{
+    this.resPromedio = this.resSuma/2;
+
+  }
+  suma():void{
+    this.resSuma= +this.edadUno + +this.edadDos;
+  }
+
+  limpiar():void{
+    this.edadUno= 0;
+    this.edadDos= 0;
+    this.resPromedio= 0;
+    this.resSuma=0
+  }
+  calcular():void{
+    this.suma();
+    this.promedio();
+    
+  }
+
 }
